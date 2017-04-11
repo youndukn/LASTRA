11a3d01cd8df7461b5b747623e2ef69f6f7ddde0
ydnam <youndukn1@gmail.com>
2017-04-11 23:59:20 +0900

    -List to tuples -fixed error in reward_calculation

--- reward_calculator.py
+++ reward_calculator.py
@@ -1,12 +1,13 @@
 from astra import Astra
+import numpy as np
 
 class RewardCalculator:
     def __init__(self, astra_input_reader):
         self.__astra_input_reader = astra_input_reader
         self.numb = 6
-        self.max = [100000, 3000, 3, 3, 3, 3]
-        self.dev = [0 for i in range(6)]
-        self.dev_p = [64.39, 22.8, 0.08, 0.004, 0.10, 0.088]
+        self.max = (100000, 3000, 3, 3, 3, 3)
+        self.dev = (0, 0, 0, 0, 0, 0)
+        self.dev_p = (64.39, 22.8, 0.08, 0.004, 0.10, 0.088)
 
 
     def calculate_rate(self):
@@ -26,7 +27,7 @@ class RewardCalculator:
             if changed and not info:
                 astra.reset()
 
-        pre_burnup, pre_cbc, pre_fr, pre_fz, pre_fxy, pre_fq = self.get_parameters(lists)
+        pre = self.get_parameters(lists)
 
         astra.reset()
 
@@ -43,19 +44,15 @@ class RewardCalculator:
             core, lists, changed, info = astra.change(oct_move, second_move)
             if changed and info:
                 number += 1
-                burnup, cbc, fr, fz, fxy, fq = self.get_parameters(lists)
+                now = self.get_parameters(lists)
 
-                dev[0] += abs(burnup - pre_burnup)
-                dev[1] += abs(cbc - pre_cbc)
-                dev[2] += abs(fr - pre_fr)
-                dev[3] += abs(fz - pre_fz)
-                dev[4] += abs(fxy - pre_fxy)
-                dev[5] += abs(fq - pre_fq)
+                for i in range(len(pre)):
+                    dev[i] += np.abs(now[i]-pre[i])
 
                 print()
-                print(burnup, cbc, fr, fxy, fq)
-                print(dev[0], dev[1], dev[2], dev[3], dev[4])
-                pre_burnup, pre_cbc, pre_fr, pre_fz, pre_fxy, pre_fq = self.get_parameters(lists)
+                print([i for i in now])
+                print([i for i in dev])
+                pre = self.get_parameters(lists)
 
             if changed and not info:
                 astra.reset()
@@ -67,21 +64,14 @@ class RewardCalculator:
 
     @staticmethod
     def get_parameters(lists):
-        burnup = 0
-        cbc = 0
-        fr = 0
-        fz = 0
-        fxy = 0
-        fq = 0
+
+        temp_list = []
 
         for a_list in lists:
             for values in a_list:
                 if len(values) > 0:
-                    burnup = float(values[1]) if burnup < float(values[1]) else burnup
-                    cbc = float(values[3]) if cbc < float(values[3]) else cbc
-                    fr = float(values[7]) if fr < float(values[7]) else fr
-                    fz = float(values[8]) if fz < float(values[8]) else fz
-                    fxy = float(values[9]) if fxy < float(values[9]) else fxy
-                    fq = float(values[10]) if fq < float(values[10]) else fq
+                    temp_list.append(values[0:14])
 
-        return burnup, cbc, fr, fz, fxy, fq
+        a = np.array(temp_list, dtype=np.float64)
+
+        return (a.max(axis=0)[1], a.max(axis=0)[3], a.max(axis=0)[7], a.max(axis=0)[8], a.max(axis=0)[9], a.max(axis=0)[10])
